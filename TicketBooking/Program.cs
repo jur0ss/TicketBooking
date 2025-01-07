bc3be230257f5aa1ac8014720f255240f817905c
jurekg <jurek.gryz2007@gmail.com>
2025-01-07 01:08:50 +0100

    Created class Concert

--- TicketBooking/Program.cs
+++ TicketBooking/Program.cs
@@ -1,11 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.InteropServices.JavaScript;
 using static System.Console;
 
 internal class Program
 {
     public class Concert
     {
+        public string Name { get; set; }
         
+        public JSType.Date Date { get; set; }
+        
+        public string Location { get; set; }
+        
+        public int AvailableSeats { get; set; }
     }
 }
